fea0be99fcecf7de834677a63a8c3980848071d5
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-07 05:05:29 +0200

    Created new java project with javainitializr with dependencies:     - Spring Web     - Spring Data JPA     - MySQL Driver     - Validation     - Lombok

---
+++ src/test/java/com/SpringbootAngular/manageServers/ManageServersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringbootAngular.manageServers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
