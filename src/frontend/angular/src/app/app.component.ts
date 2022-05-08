c02bd9c70da9229494ce339ce66dc7c1a7cb884c
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-08 02:59:16 +0200

    App component now makes a request to serverService to get all servers. App component view shows http response of requesting servers. Deleted tests for app component.

--- src/frontend/angular/src/app/app.component.ts
+++ src/frontend/angular/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from './enum/data.state.enum';
+import { AppState } from './interface/app-state.interface';
+import { CustomResponse } from './interface/custom-response.interface';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit{
+  appState$: Observable<AppState<CustomResponse>>;
+  constructor(private serverSvc: ServerService){}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverSvc.servers$
+    .pipe(
+      map(response => {
+        return { dataState: DataState.LOADED_STATE, appData: response}
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error })
+      })
+    );
+  }
 }
