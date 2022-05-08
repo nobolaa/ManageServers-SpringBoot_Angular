73e75436e0954570f99a03894973f6164b932c24
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-08 04:22:49 +0200

    Content of app-component is now being displayed dynamically:     - Shows (loading | servers | error depending) of the data state     - Show servers requested from API     - Shows (server up | server) down depending of server status

--- src/frontend/angular/src/app/app.component.ts
+++ src/frontend/angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { catchError, map, Observable, of, startWith } from 'rxjs';
 import { DataState } from './enum/data.state.enum';
+import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state.interface';
 import { CustomResponse } from './interface/custom-response.interface';
 import { ServerService } from './service/server.service';
@@ -12,6 +13,9 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit{
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+
   constructor(private serverSvc: ServerService){}
 
   ngOnInit(): void {
